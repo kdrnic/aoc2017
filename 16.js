70599b0aa2f1e7f2601d081abdbb0c3b7e0e9df5
kdrnic <lucasdealmeidasm@gmail.com>
2017-12-16 22:45:21 -0200

    so dum

--- 16.js
+++ 16.js
@@ -6,20 +6,85 @@ progsInLine = Array((ex ? "e" : "p").charCodeAt(0) - "a".charCodeAt(0) + 1).fill
 progs = progsInLine.slice();
 moves = {
 	s: a => progs.unshift.apply(progs, progs.splice(-parseInt(a))),
-	x:	(a,b) => progs = progs.map((e, i) => i == parseInt(a) ? progs[parseInt(b)] : i == parseInt(b) ? progs[parseInt(a)] : e),
-	p:	(a,b) => progs = progs.map((e, i) => e == a ? b : e == b ? a : e)
+	x:	(a, b) => progs = progs.map((e, i) => i == parseInt(a) ? progs[parseInt(b)] : i == parseInt(b) ? progs[parseInt(a)] : e),
+	p:	(a, b) => progs = progs.map((e, i) => e == a ? b : e == b ? a : e)
 };
+
 dance.forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
 console.log(progs.join(""));
+/*
+dance.forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+console.log(2, progs.join(""));
+dance.forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+console.log(3, progs.join(""));
+dance.forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+console.log(4, progs.join(""));
+dance.forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+console.log(5, progs.join(""));
+*/
+progs = progsInLine.slice();
+dance.filter(m => m[0] != "p").forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+permutationNoP = progs.map(e => progsInLine.indexOf(e));
+progsNoP = progs;
+progs = progsInLine.slice();
+dance.filter(m => m[0] == "p").forEach(m => moves[m[0]](m.substr(1).split("/")[0], m.split("/")[1]));
+permutationOnlyP = progs.slice();
+progsOnlyP = progs;
+/*
+progs = progsInLine.slice();
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(1, progs.join(""));
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(2, progs.join(""));
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(3, progs.join(""));
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(4, progs.join(""));
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(5, progs.join(""));
+*/
 
 total = 1;
-
 for(var done = 1; done < 1000000000; ){
-	permutation = progs.map(e => progsInLine.indexOf(e));
+	permutationNoP = progsNoP.map(e => progsInLine.indexOf(e));
+	permutationOnlyP = progsOnlyP.slice();
 	for(i = 1; i < 10; i++){
-		progs = permutation.map(e => progs[e]);
+		progsNoP = permutationNoP.map(e => progsNoP[e]);
+		progsOnlyP = progsOnlyP.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+		//progs = permutation.map(e => progs[e]);
 		total += done;
-		console.log(total, progs.join(""));
 	}
 	done = total;
 }
+progs = progsInLine.slice();
+progs = permutationNoP.map(e => progs[e]);
+progs = progs.map(e => permutationOnlyP[progsInLine.indexOf(e)]);
+console.log(progs.join(""));
+
+/*
+LET THIS IDIOCY BE REMEMBERED:
+moves2 = {
+	s: a => "progs.unshift.apply(progs, progs.splice(-" + a +"));",
+	x:	(a,b) => "temp = progs[" + a + "]; progs[" + a + "] = progs[" + b + "]; progs[" + b + "] = temp;",
+	p:	(a,b) => "temp = progs.indexOf(\"" + a + "\"); progs[progs.indexOf(\"" + b + "\")] = \"" + a +"\"; progs[temp] = \"" + b + "\";"
+};
+danceFuncSrc = dance.map(m => moves2[m[0]](m.substr(1).split("/")[0], m.split("/")[1])).join("");
+danceFuncSrc = Array(1).fill(danceFuncSrc).join("");
+danceFunc = new Function(danceFuncSrc);
+if(ex){
+	progs = progsInLine.slice();
+	danceFunc();
+	console.log(progs.join(""));
+}
+progs = progsInLine.slice();
+for(var done = 0; done < 1000000000; done += 1){
+	danceFunc();
+	if(done % 10000000 == 0) console.log(Math.floor(done / 10000000) + "%");
+}
+console.log(progs.join(""));
+*/
