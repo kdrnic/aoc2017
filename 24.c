cd4d9b63b4cd6be5169cfac7000231867387696f
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2017-12-24 19:19:23 -0200

    last 3 expansions version

--- 24.c
+++ 24.c
@@ -20,44 +20,56 @@ int is_heavier(bridge *a, bridge *b)
 	return 0;
 }
 
+#define bridge_length(a) (__builtin_popcount((unsigned int) ((a).c & cast64(0xFFFFFFFF))) + \
+	__builtin_popcount((unsigned int) ((a).c >> cast64(32))))
+int is_longer(bridge *a, bridge *b)
+{
+	if(bridge_length(*a) > bridge_length(*b)) return 1;
+	return 0;
+}
+
+int good_length = 0;
+
+int is_gooder(bridge *a, bridge *b)
+{
+	if(bridge_length(*a) == good_length)
+	{
+		if(bridge_length(*b) == good_length)
+		{
+			if(a->w > b->w) return 1;
+			return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
+
 int (*is_better)(bridge *, bridge *); // function pointer for comparison function
 
-int queue_length = 0;
 bridge expand_bridge(bridge *b)
 {
-	bridge topb = {0, 0, -999};
+	bridge topb = *b;
 	int i;
 	uint64_t j;
 	
 	for(i = 0, j = 1; i < num_comps; i++, j = j << cast64(1))
 	{
-		bridge newb = {0, 0, 0};
+		bridge newb = *b;
 		
 		if(b->c & j) continue; // this component has been used
 		if(!(comps[i] & b->p)) continue; // component doesn't have port
 		
-		topb.w = -988;
-		
-		newb = *b;
 		newb.w += compw[i];
 		newb.c |= j; // use this component
 		newb.p = ~(b->p);
 		newb.p &= comps[i]; // tries to find the other port of the component
 		if(!newb.p) newb.p = b->p; // in case component has two same ports
 		
-		queue_length++;
 		newb = expand_bridge(&newb);
 		if(is_better(&newb, &topb)) topb = newb;
 	}
 	
-	//queue_length--;
 	return topb;
-}
-
-void print_bridge(bridge *b)
-{
-	printf("w: %04d   p:%"PRIu64" c:%"PRIu64"\n", b->w, b->p, b->c);
-	fflush(stdout);
 }
 
 int main()
@@ -75,14 +87,21 @@ int main()
 	{
 		comps[num_comps] = (cast64(1) << cast64(porta)) | (cast64(1) << cast64(portb));
 		compw[num_comps] = porta + portb;
-		printf("%02d/%02d %04d %"PRIu64"\n", porta, portb, compw[num_comps], comps[num_comps]);
+		//printf("%02d/%02d %04d %"PRIu64"\n", porta, portb, compw[num_comps], comps[num_comps]);
 		num_comps++;
 	}
 	
 	is_better = &is_heavier;
-	print_bridge(&startb);
 	heavyb = expand_bridge(&startb);
-	printf("%d %d\n", heavyb.w, queue_length);
+	printf("%d\n", heavyb.w);
+	
+	is_better = &is_longer;
+	longb = expand_bridge(&startb);
+	good_length = bridge_length(longb);
+	is_better = &is_gooder;
+	bestb = expand_bridge(&startb);
+	
+	printf("l: %d, w: %d\n", good_length, bestb.w);
 	
 	return 0;
 }
