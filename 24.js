e4bad587ff230a9eb75003d540df8ba19826e943
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2017-12-24 16:42:01 -0200

    slowpotato

--- 24.js
+++ 24.js
@@ -2,48 +2,56 @@ fs = require("fs");
 inp = fs.readFileSync("input24.txt").toString().split("\n").filter(e => e.length);
 comps = inp.map(l => l.split("/").map(n => parseInt(n)));
 
-queue = [{p: [0], c: comps, w: 0}];
+if(!fs.existsSync("./24.json")){
+	console.log("Bridges not found, generating...");
+	
+	queue = [{p: [0], c: comps, w: 0}];
+	bridges = [];
+	it = 0;
+	interv = 1000;
+	//console.time("" + (it + interv));
+	while(queue.length){
+		var b = queue.shift();
 
-bridges = [];
-it = 0;
-interv = 1000;
-console.time("" + (it + interv));
-while(queue.length){
-	var b = queue.shift();
-	
-	if(b.p.length + b.c.length - 1 != comps.length){
-		console.log(b);
-		process.exit();
-	}
-	if(it % interv == 0){
-		var oldQl = queue.length;
-		//FilterQueue();
-		console.log(queue.length, oldQl - queue.length, bridges.length);
-		console.timeEnd("" + (it));
-		console.time("" + (it + interv));
-	}
-	it++;
-	
-	var port = b.p[b.p.length - 1];
-	var cs = b.c.filter(c => c.includes(port));
-	if(!cs.length){
-		bridges.push(b);
-		continue;
-	}
-	var newQs = cs.map(function(e, i){
-		var newSeg = {
-			p: b.p.slice(),
-			c: b.c.slice(),
-			w: b.w + e[0] + e[1]
+		if(b.p.length + b.c.length - 1 != comps.length){
+			console.log(b);
+			process.exit();
 		}
-		newSeg.p.push(e[0] == port ? e[1] : e[0]);
-		newSeg.c.splice(newSeg.c.indexOf(e), 1);
-		return newSeg;
-	});
-	queue = queue.concat(newQs);
+		if(it % interv == 0){
+			//var oldQl = queue.length;
+			//FilterQueue();
+			//console.log(queue.length, oldQl - queue.length, bridges.length);
+			//console.timeEnd("" + (it));
+			//console.time("" + (it + interv));
+		}
+		it++;
+
+		var port = b.p[b.p.length - 1];
+		var cs = b.c.filter(c => c.includes(port));
+		if(!cs.length){
+			bridges.push(b);
+			continue;
+		}
+		var newQs = cs.map(function(e, i){
+			var newSeg = {
+				p: b.p.slice(),
+				c: b.c.slice(),
+				w: b.w + e[0] + e[1]
+			}
+			newSeg.p.push(e[0] == port ? e[1] : e[0]);
+			newSeg.c.splice(newSeg.c.indexOf(e), 1);
+			return newSeg;
+		});
+		queue = queue.concat(newQs);
+	}
+	fs.writeFileSync("24.json", JSON.stringify(bridges));
 }
-bridges.sort((a, b) => a.w - b.w);
-console.log(bridges.shift().w, bridges.pop().w);
+else {
+	bridges = JSON.parse(fs.readFileSync("24.json").toString());
+}
+console.log(bridges.reduce((acc, e) => acc > e.w ? acc : e.w, 0));
+longestL = bridges.reduce((acc, e) => acc > e.p.length ? acc : e.p.length, 0);
+console.log(bridges.filter(b => b.p.length == longestL).reduce((acc, e) => acc > e.w ? acc : e.w, 0));
 /*
 function CompSort(a, b){
 	var x = a.slice();
