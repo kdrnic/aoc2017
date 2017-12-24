2e6640d8b05c739e486eee1430a7d129df36fbf9
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2017-12-24 16:28:32 -0200

    slowpotato

--- 24.js
+++ 24.js
@@ -2,46 +2,73 @@ fs = require("fs");
 inp = fs.readFileSync("input24.txt").toString().split("\n").filter(e => e.length);
 comps = inp.map(l => l.split("/").map(n => parseInt(n)));
 
-// Sorts a component
-Func = (a, b) => a[0] == b ? [a[0], a[1]] : [a[1], a[0]];
-// Gets the last added component
-Func3 = a => a.p[a.p.length - 1];
-// Gets the second last added component's port, or 0
-Func5 = a => a.p.length >= 2 ? a.p[a.p.length - 2] : 0;
-// Copies into a component
-function Func4(a, b){
-	a[0] = b[0];
-	a[1] = b[1];
-}
+queue = [{p: [0], c: comps, w: 0}];
 
-queue = comps.filter(c => c.includes(0)).map(c => ({p: [c], c: comps.slice()}));
-
-// Removes the used component from the list of components, sort the used component
-function Func2(qq){
-	qq.forEach(q => q.c.splice(q.c.indexOf(Func3(q)), 1));
-	qq.forEach(q => Func4(Func3(q), Func(Func3(q), Func5(q))));
-}
-Func2(queue);
-console.log(queue);
 bridges = [];
 it = 0;
+interv = 1000;
+console.time("" + (it + interv));
 while(queue.length){
 	var b = queue.shift();
 	
-	if(b.p.length + b.c.length != comps.length){
+	if(b.p.length + b.c.length - 1 != comps.length){
 		console.log(b);
 		process.exit();
 	}
-	if(it++ % 1000 == 0) console.log(queue.length, bridges.length);
+	if(it % interv == 0){
+		var oldQl = queue.length;
+		//FilterQueue();
+		console.log(queue.length, oldQl - queue.length, bridges.length);
+		console.timeEnd("" + (it));
+		console.time("" + (it + interv));
+	}
+	it++;
 	
-	var port = b.p[b.p.length - 1][1];
+	var port = b.p[b.p.length - 1];
 	var cs = b.c.filter(c => c.includes(port));
 	if(!cs.length){
 		bridges.push(b);
 		continue;
 	}
-	cs = cs.map(c => Func(c, port));
-	var newQs = cs.map(c => ({p: b.p.concat([c]), c: b.c.slice()}));
-	Func2(newQs);
+	var newQs = cs.map(function(e, i){
+		var newSeg = {
+			p: b.p.slice(),
+			c: b.c.slice(),
+			w: b.w + e[0] + e[1]
+		}
+		newSeg.p.push(e[0] == port ? e[1] : e[0]);
+		newSeg.c.splice(newSeg.c.indexOf(e), 1);
+		return newSeg;
+	});
 	queue = queue.concat(newQs);
 }
+bridges.sort((a, b) => a.w - b.w);
+console.log(bridges.shift().w, bridges.pop().w);
+/*
+function CompSort(a, b){
+	var x = a.slice();
+	var y = b.slice();
+	if(x[0] > x[1]) x.reverse();
+	if(y[0] > y[1]) y.reverse();
+}
+
+function BridgeEq(a, b){
+	if(a.p[a.p.length - 1] != b[b.p.length - 1]) return false;
+	var ca = a.c.slice().sort(CompSort);
+	var cb = b.c.slice().sort(CompSort);
+	if(JSON.stringify(ca) != JSON.stringify(cb)) return false;
+	return true;
+}
+
+function FilterQueue(){
+	var newQueue = [];
+	for(var i = 0; i < queue.length; i++){
+		var j;
+		for(j = 0; j < newQueue.length; j++){
+			if(BridgeEq(queue[i], newQueue[j])) break;
+		}
+		if(j == newQueue.length) newQueue.push(queue[i]);
+	}
+	queue = newQueue;
+}
+*/
