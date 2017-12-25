6e08bd4b84da5bfbce812de96d681be951c1627f
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2017-12-24 22:25:07 -0200

    last naive for part 2

--- 21.js
+++ 21.js
@@ -16,11 +16,11 @@ function Extract(x, y, s){
 }
 
 ops = [
-	RotCw = (x, y, s) => [s - y, x, s],		//rotcw
+	RotCw = (x, y, s) => [s - y - 1, x, s],		//rotcw
 	RotCw2 = (x, y, s) => RotCw.apply(null, RotCw(x, y, s)), // rotcw twice
 	RotCw3 = (x, y, s) => RotCw.apply(null, RotCw.apply(null, RotCw(x, y, s))),  //rotcw thrice
-	Vflip = (x, y, s) => [s - x, y, s], // vflip
-	Hflip = (x, y, s) => [x, s - y, s], // hflip
+	Vflip = (x, y, s) => [s - x - 1, y, s], // vflip
+	Hflip = (x, y, s) => [x, s - y - 1, s], // hflip
 	Id = (x, y, s) => [x, y, s],
 	RotV = (x, y, s) => RotCw.apply(null, Vflip(x, y, s)),
 	RotV2 = (x, y, s) => RotCw2.apply(null, Vflip(x, y, s)),
@@ -35,7 +35,9 @@ function ApplyOp(p, s, op){
 			var v = p[m[x + y * s]];
 			var _x = op(x, y, s)[0];
 			var _y = op(x, y, s)[1];
-			res[m[_x + _y * s]] = v;
+			var idx = _x + _y * s;
+			if((idx >= m.length) || ((v != ".") && (v != "#")) || (!v)) console.log("error", idx, _x, _y, s, v);
+			res = res.substr(0, m[idx]) + v + res.substr(m[idx] + 1);
 		}
 	}
 	return res;
@@ -56,35 +58,45 @@ function Reconstitute(nps, s){
 		for(var x = 0; x < size; x++){
 			var xr = x % s;
 			var idx = Math.floor(x / s) + Math.floor(y / s) * (size / s);
-			
+			if(!nps[idx]) console.log(nps, idx);
 			res += nps[idx][m[xr + yr * s]];
 		}
 	}
 	return res;
 }
 
-for(var i = 0; i < 5; i++){
+for(var i = 0; i < 18; i++){
+	console.time(i);
 	var s = 0;
 	if(size % 2 == 0) s = 2;
-	if(size % 3 == 0) s = 3;
+	else if(size % 3 == 0) s = 3;
 	var nps = [];
+	
+	//console.log("          ", i, size, s);
+	
 	for(var y = 0; y < size / s; y++){
 		for(var x = 0; x < size / s; x++){
 			var np = Extract(x * s, y * s, s);
 			var bk = GenOps(np, s).filter(p => book.has(p))[0];
+			if(!bk) console.log(np, GenOps(np,s));
 			nps.push(book.get(bk));
 		}
 	}
 	
-	if(size % 2 == 0){
+	if(s == 2){
 		size = (size / s) * 3;
 		s = 3;
 	}
-	if(size % 3 == 0){
+	else if(s == 3){
 		size = (size / s) * 4;
 		s = 4;
 	}
 	
 	pattern = Reconstitute(nps, s);
+	
+	if(i + 1 == 5) console.log(pattern.match(/#/g).length);
+	if(i + 1 == 18) console.log(pattern.match(/#/g).length);
+	
+	console.timeEnd(i);
 }
-console.log(pattern.match(/#/g).length);
+//console.log(pattern.replace(/\//g, "\n"));
